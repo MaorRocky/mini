673cfab35761994141b891a9b2e9726503b7d326
Maor Rocky <maorrocky@gmail.com>
2020-05-13 23:38:14 +0300

    Update sc.py

--- sc.py
+++ sc.py
@@ -1,12 +1,19 @@
 # %%
 from pathlib import Path
 import community
-
+import numpy as np
 import networkx as nx
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
-plt.figure(figsize=(20, 15))
+
+# function to sort dics by values
+def sort_dic(dic):
+    return {k: v for k, v in sorted(dic.items(), key=lambda item: item[1])}
+
+
+# raeding data
 data_name = 'Obf_oneInTenWeek1_d'
 suffix = '.tsv'
 G = nx.Graph()
@@ -15,73 +22,66 @@ G = nx.Graph()
 data = pd.read_csv(Path().joinpath('data', data_name + str(1) + suffix), sep='\t',
                    error_bad_lines=False, index_col=False, dtype='unicode')
 
-print(len(data))
-
-name = data.columns[0]
+print('num of rows in data', len(data))
+# %%
+# instead of using names we will use sha1
+# Number of distinct machines file was downloaded to from this domain. this will be the weight of and edge
+# name = data.columns[0]
+start = time.time()
+sha1 = data.columns[3]
 domain = data.columns[17]
 machine = data.columns[13]
 data = data.sort_values(by=data.columns[0])
-machine_dic = {}
+machines_dic = {}
 for index, row in data.iterrows():
-    fileName = row[name]
+    file_sha1 = row[sha1]
     file_domain = row[domain]
-    machine_dic[(fileName, file_domain)] = machine_dic.get((fileName, file_domain), 0) + 1
-# machine_dic = {k: v for k, v in sorted(machine_dic.items(), key=lambda item: item[1])}
-# print(machine_dic)
+    machine_guid = row[machine]
+    machines_dic[(file_sha1, file_domain)] = machines_dic.get((file_sha1, file_domain), []) + [machine_guid]
 
+for key, val in machines_dic.items():
+    machines_dic[key] = len(list(set(val)))
+machines_dic = sort_dic(machines_dic)
 
 for index, row in data.iterrows():
-    fileName = row[name]
+    file_sha1 = row[sha1]
     file_domain = row[domain]
-    G.add_edge(fileName, file_domain, weight=machine_dic[(fileName, file_domain)])
-
+    G.add_edge(file_sha1, file_domain, weight=machines_dic[(file_sha1, file_domain)])
+print('It took {} seconds.'.format(time.time() - start))
+print("Num of nodes in G {}".format(len(G)))
 # %%
 attr = nx.get_edge_attributes(G, 'weight')
-# attr = {key: value for (key, value) in attr.items() if value > 100}
-attr = {k: v for k, v in sorted(attr.items(), key=lambda item: item[1])}
+attr = sort_dic(attr)
 for key, value in attr.items():
-    print(key, value)
+    print(key, ' : ', value)
 print("len is ", len(attr))
+# %%
+partition = community.best_partition(G, weight='weight')
+partition = sort_dic(partition)
 
-# %%
-lst = {}
-print("Num of nodes in G {}".format(len(G)))
-# for tup in nx.degree(G):
-#     lst[tup[0]] = tup[1]
-# lst = {k: v for k, v in sorted(lst.items(), key=lambda item: item[1])}
-# print(lst)
-# Find modularity
-partition = community.best_partition(G)
-partition = {k: v for k, v in sorted(partition.items(), key=lambda item: item[1])}
+# file_names_column = data[data.columns[0]]
+# fileName_set = set(file_names_column)
 
-file_names_column = data[data.columns[0]]
-fileName_set = set(file_names_column)
-# %%
-for key, val in partition.items():
-    print(key, ':', val)
-# %%
+file_sah1_column = data[data.columns[3]]
+file_sah1_set = set(file_sah1_column)
 domain_per_cluster = {}
 files_per_cluster = {}
 for key, val in partition.items():
-    if key in fileName_set:
+    if key in file_sah1_set:
         files_per_cluster[val] = files_per_cluster.get(val, []) + [key]
     else:
         domain_per_cluster[val] = domain_per_cluster.get(val, []) + [key]
 
-# files_per_cluster = {k: v for k, v in sorted(files_per_cluster.items(), key=lambda item: item[1])}
-
-for key, val in domain_per_cluster.items():
-    print(key, ':', val)
-
 # %%
 threat = data.columns[20]
-name = data.columns[0]
+# name = data.columns[0]
+sha1 = data.columns[3]
 
 threat_files_set = set()
 for index, row in data.iterrows():
     file_threat = row[threat]
     if isinstance(file_threat, str):
-        threat_files_set.add(row[name])
+        threat_files_set.add(row[sha1])
 
 threat_column = data[data.columns[20]]
 dirty_files_lst = []
@@ -95,16 +95,36 @@ for file_list in files_per_cluster.values():
 print(dirty_files_lst)
 # %%
 machines_per_cluster = {}
-index = 0
-for files_list, domains_list in zip(files_per_cluster.values(), domain_per_cluster.values()):
+for index, (files_list, domains_list) in enumerate(zip(files_per_cluster.values(), domain_per_cluster.values())):
     for file in files_list:
         for domain in domains_list:
             if G.has_edge(file, domain):
                 machines_per_cluster[index] = machines_per_cluster.get(index, 0) + G[file][domain]['weight']
-    index += 1
 
-machines_per_cluster = {k: v for k, v in sorted(machines_per_cluster.items(), key=lambda item: item[1])}
+machines_per_cluster = sort_dic(machines_per_cluster)
 
-for key, value in machines_per_cluster.items():
-    print(key, value)
+print(*machines_per_cluster.items(), sep='\n')
 # %%
+plt.figure(figsize=(30, 20))
+t = np.arange(0., len(dirty_files_lst), 1)
+
+# plt.plot(t, dirty_files_lst, 'r--', )
+plt.scatter(np.arange(0.0, len(dirty_files_lst), 1.0), dirty_files_lst)
+plt.xlabel('cluster number')
+plt.savefig('graph.png')
+plt.show()
+# %%
+
+t = np.arange(0., len(files_per_cluster), 1)
+plt.figure(figsize=(10, 5))
+
+files_per_cluster_lst = []
+for key, value in files_per_cluster.items():
+    files_per_cluster_lst.append(len(value))
+# plt.plot(t, dirty_files_lst, 'r--', )
+plt.scatter(np.arange(0.0, len(files_per_cluster), 1.0), files_per_cluster_lst, linewidth=1)
+plt.xlabel('cluster number')
+plt.ylabel('files per cluster')
+plt.xscale('log')
+plt.savefig('graph.png')
+plt.show()
